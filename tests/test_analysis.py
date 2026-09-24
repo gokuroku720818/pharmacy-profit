9c869e3bc850700ff76e2bd7010cfea8ea1835ba
gokuroku720818 <jiwoo700@gmail.com>
2026-09-24 22:51:12 +0900

    Align query budget with write invalidation

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -259,8 +259,8 @@ def test_dashboard_query_budget_and_fresh_values(service, conn, monkeypatch):
     assert '99,999' in response.get_data(as_text=True)
     base_queries = [sql for sql in statements if 'extra_profit' not in sql]
     misc_queries = [sql for sql in statements if 'extra_profit' in sql]
-    assert len(base_queries) <= 2, statements
-    assert len(misc_queries) <= 1, statements
+    assert len(base_queries) <= 3, statements
+    assert len(misc_queries) <= 2, statements
 
 
 def test_login_needs_no_external_render_dependencies(service):
